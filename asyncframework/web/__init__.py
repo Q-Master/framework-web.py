59894196d829ecf66d68833157ca4e8c1c57dfb4
Vladimir Berezenko <qmaster2000@gmail.com>
2023-03-27 17:31:13 +0300

    Fixed for newer build systems.

--- asyncframework/web/__init__.py
+++ asyncframework/web/__init__.py
@@ -4,3 +4,17 @@ from .web_client import *
 from .web_request import *
 from .web_response import *
 from .web_service import *
+
+__version__ = '0.1.1'
+
+__title__ = 'asyncframework-web'
+__description__ = 'Async framework web addon.'
+__url__ = 'https://github.com/Q-Master/framework-web.py'
+__uri__ = __url__
+__doc__ = f"{__description__} <{__uri__}>"
+
+__author__ = 'Vladimir Berezenko'
+__email__ = 'qmaster2000@gmail.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019-2023 Vladimir Berezenko'
